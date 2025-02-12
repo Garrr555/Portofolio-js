f73cb7419e64dd5cc49de5613e4aad8b0754ffbb
Mohammad Tegar Ibrahim <mohammadtegaribrahim@gmail.com>
2025-02-12 19:18:27 +0700

    services

--- src/app/(navbar)/services/page.jsx
+++ src/app/(navbar)/services/page.jsx
@@ -1,5 +1,42 @@
-export default function Services (){
-    return(
-        <div>Services Page</div>
-    )
+"use client";
+
+import { BsArrowDownRight } from "react-icons/bs";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import services from "@/data/services";
+
+export default function Services() {
+  return (
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+      <div className="container mx-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+          }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+        >
+          {services.map((service, i) => {
+            return (
+              <div
+                key={i}
+                className="flex-1 flex flex-col justify-center gap-6 group"
+              >
+                <div className="flex w-full justify-between items-center">
+                  <div>{service.num}</div>
+                  <Link href={service.href}>
+                    <BsArrowDownRight />
+                  </Link>
+                </div>
+                <h2>{service.title}</h2>
+                <p>{service.description}</p>
+                <div className="w-full border-b border-white/20"></div>
+              </div>
+            );
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
 }
---
+++ src/data/services.js
@@ -0,0 +1,32 @@
+const services = [
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ratione deleniti incidunt quia assumenda tenetur, enim nesciunt!",
+    href: "/",
+  },
+  {
+    num: "02",
+    title: "Web Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ratione deleniti incidunt quia assumenda tenetur, enim nesciunt!",
+    href: "/",
+  },
+  {
+    num: "03",
+    title: "Web Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ratione deleniti incidunt quia assumenda tenetur, enim nesciunt!",
+    href: "/",
+  },
+  {
+    num: "04",
+    title: "Web Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ratione deleniti incidunt quia assumenda tenetur, enim nesciunt!",
+    href: "/",
+  },
+];
+
+export default services
